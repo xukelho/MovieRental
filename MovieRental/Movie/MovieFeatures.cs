a41190402e05a0f28f0792f4d198accc32e85853
Coelho-MSI <Coelho-MSI@DESKTOP-M6JI0KR>
2026-01-07 11:27:29 +0000

    Added comment on the MovieFeatures.GetAll() method

--- MovieRental/Movie/MovieFeatures.cs
+++ MovieRental/Movie/MovieFeatures.cs
@@ -17,8 +17,11 @@ namespace MovieRental.Movie
 			return movie;
 		}
 
-		// TODO: tell us what is wrong in this method? Forget about the async, what other concerns do you have?
-		public List<Movie> GetAll()
+        // TODO: tell us what is wrong in this method? Forget about the async, what other concerns do you have?
+        // A method like this shouldn't exist. It has a high risk of returning a large amount of data, which can lead to performance issues and increased memory usage.
+        // There should be some sort of filterting, pagination, or another way to limit the amount of data returned.
+        // Also, depending for what it is needed for, consider returning a dto with the necessary fields only, instead of the full entity.
+        public List<Movie> GetAll()
 		{
 			return _movieRentalDb.Movies.ToList();
 		}
